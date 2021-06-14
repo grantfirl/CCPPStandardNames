7d62693e3ca9bd94a5ae96bdf012dd13b97f6c78
grantfirl <grantf@ucar.edu>
2021-06-14 14:10:40 -0600

    add new utility script for dealing with standard names

---
+++ standard_name_helper.py
@@ -0,0 +1,299 @@
+#!/usr/bin/env python
+
+"""
+This script has several functions: 
+1) check standard names from a set of host/physics metadata against the CCPPStandardNames XML list (and writes out unknown names)
+2) determines the changes between two CCPPStandardNames XML lists (and writes out change files with new, replaced, and removed names)
+3) bullet 2 + performing the replacement of standard names in a set of host/physics metadata (either in temporary files or overwriting existing files)
+
+Needs: ccpp-framework as directory adjacent to script
+"""
+
+import argparse
+import sys
+import os.path
+from xml_tools import read_xml_file
+sys.path.insert(1, './ccpp-framework/scripts')
+from ccpp_prebuild import import_config, gather_variable_definitions, collect_physics_subroutines
+import difflib
+import json
+import re
+import copy
+
+# Default path to the current (or changed) standard name dictionary
+DEFAULT_STD_NM_XML = 'standard_names.xml'
+
+# Default path to the old standard name dictionary
+DEFAULT_OLD_STD_NM_XML = 'standard_names_old.xml'
+
+# Filename of JSON file with list of host variables found in metadata but not in the standard name list (will not write if string is empty)
+#JSON_UNKNOWN_HOST_FILE = ''
+JSON_UNKNOWN_HOST_FILE = 'unknown_host_vars.json'
+
+# Filename of JSON file with list of physics variables found in metadata but not in the standard name list (will not write if string is empty)
+#JSON_UNKNOWN_PHYSICS_FILE = ''
+JSON_UNKNOWN_PHYSICS_FILE = 'unknown_physics_vars.json'
+
+# Filename of JSON file with list of standard name changes (will not write if string is empty)
+#JSON_CHG_FILE = ''
+JSON_CHG_FILE = 'standard_name_changes.json'
+
+TEMP_TAG = '_meta_chg'
+
+###############################################################################
+def parse_command_line(args, description):
+###############################################################################
+    parser = argparse.ArgumentParser(description=description,
+                                     formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('-m', '--mode', help='script mode (u=check unknown variables, c=check changed variables, m=check changed variables and modify metadata)', 
+                        type=str, choices=('u','c','m'), required=True)
+    parser.add_argument("config_file",
+                        metavar='<config file>',
+                        type=str, help="ccpp_prebuild_config.py file")
+    parser.add_argument("host_base",
+                        metavar='<host base dir>',
+                        type=str, help="top-level directory for host model")
+    parser.add_argument('-o', '--old_standard_name_file',
+                        metavar='<old standard names filename>',
+                        type=str, help="old XML file with standard name library",required=False,
+                        default=DEFAULT_OLD_STD_NM_XML)
+    parser.add_argument('-f', '--standard_name_file',
+                        metavar='<standard names filename>',
+                        type=str, help="XML file with (current or changed) standard name library",
+                        required=False,default=DEFAULT_STD_NM_XML)
+    parser.add_argument('-ov','--overwrite',     help='flag to overwrite metadata defined in the ccpp_prebuild_config.py file', action='store_true')
+    
+    pargs = parser.parse_args(args)
+    return pargs
+
+###############################################################################
+def check_unknown_vars(all_std_names, metadata_define, metadata_request, std_nm_file):
+###############################################################################
+    known_host_vars = []
+    unknown_host_vars = []
+    for k, v in metadata_define.items():
+        if k in all_std_names:
+            known_host_vars.append(k)
+        else:
+            unknown_host_vars.append(k)
+            
+    known_physics_vars =[]
+    unknown_physics_vars = []
+    for k, v in metadata_request.items():
+        if k in all_std_names:
+            known_physics_vars.append(k)
+        else:
+            unknown_physics_vars.append(k)
+    
+    print('{} of {} host variables were not found in {}'.format(len(unknown_host_vars),len(metadata_define),std_nm_file))
+    for var in unknown_host_vars:
+        print('unknown host var: {}'.format(var))
+    if JSON_UNKNOWN_HOST_FILE:
+        with open(JSON_UNKNOWN_HOST_FILE, 'w') as filehandle:
+            json.dump(unknown_host_vars, filehandle)
+    
+    print('{} of {} physics variables were not found in {}'.format(len(unknown_physics_vars),len(metadata_request),std_nm_file))
+    for var in unknown_physics_vars:
+        print('unknown physics var: {}'.format(var))
+    if JSON_UNKNOWN_PHYSICS_FILE:
+        with open(JSON_UNKNOWN_PHYSICS_FILE, 'w') as filehandle:
+            json.dump(unknown_physics_vars, filehandle)
+
+###############################################################################
+def find_changed_vars(old_file, new_file):
+###############################################################################
+    with open(old_file) as file_1:
+        file_1_text = file_1.readlines()
+  
+    with open(new_file) as file_2:
+        file_2_text = file_2.readlines()
+  
+    diff_lines = difflib.unified_diff(file_1_text, file_2_text, fromfile=old_file, 
+        tofile=new_file, lineterm='',n=0)
+    
+    std_name_changes = []
+    n_replacements = 0
+    n_adds = 0
+    n_removals = 0
+    for line in (diff_lines):
+        if '@@' in line:
+            #figure out how many lines that correspond to the hunk
+            if (',') in line: #if one comma on hunk line, there should be 2
+                n_lines_minus = int(line.split(',')[1].split(' ')[0])
+                n_lines_plus  = int(line.split(',')[2].split(' ')[0])
+            else:
+                #only one line has changed, so expect the old and new lines only
+                n_lines_minus = 1
+                n_lines_plus  = 1
+            n_lines = n_lines_minus + n_lines_plus
+            
+            #std_name_chg = []
+            if (n_lines_minus == n_lines_plus):
+                #expect one-for-one replacement
+                text_minus = []
+                text_plus = []
+                for i in range(n_lines_minus):
+                    text_minus.append(next(diff_lines))
+                for i in range(n_lines_plus):
+                    text_plus.append(next(diff_lines))
+                for i in range(len(text_minus)):
+                    if text_minus[i][0] == '-' and text_plus[i][0] == '+' and 'standard_name name=' in text_minus[i] and 'standard_name name=' in text_plus[i]:
+                        std_name_changes.append((text_minus[i].split('"')[1],text_plus[i].split('"')[1]))
+                        n_replacements += 1
+            elif (n_lines_minus == 0):
+                #expect new names only
+                for i in range(n_lines_plus):
+                    text = next(diff_lines)
+                    if 'standard_name name=' in text:
+                        std_name_changes.append(('',text.split('"')[1]))
+                        n_adds += 1
+            elif (n_lines_plus == 0):
+                #expect deleted names only
+                for i in range(n_lines_minus):
+                    text = next(diff_lines)
+                    if 'standard_name name=' in text:
+                        std_name_changes.append((text.split('"')[1],''))
+                        n_removals += 1
+            else:
+                print('Unexpected change encountered between {} and {}: {}'.format(old_file, new_file, line))
+                print('This change will not be parsed.')
+    return (std_name_changes, n_replacements, n_adds, n_removals)
+
+###############################################################################
+def edit_config_metadata(config_file, std_nm_replacements, overwrite):
+###############################################################################
+    with open(config_file,'r') as f:
+        #only replace standard names in the OPTIONAL_ARGUMENTS dictionary
+        newlines = []
+        optional_args_start = -999
+        optional_args_end = -999
+        for i, line in enumerate(f.readlines()):
+            newlines.append(line)
+            if ("OPTIONAL_ARGUMENTS = " in line):
+                optional_args_start = i
+            if ("STATIC_API_DIR = " in line):
+                optional_args_end = i
+    
+    origlines = copy.copy(newlines)
+    
+    if optional_args_start > 0 and optional_args_end > 0:
+        for r in std_nm_replacements:
+            newlines[optional_args_start:optional_args_end] = [re.sub(r'\b%s\b' % r[0], r[1], line) for line in newlines[optional_args_start:optional_args_end]]
+    
+        if newlines != origlines:
+            if overwrite:
+                new_filename = config_file
+            else:
+                (fn_root, fn_ext) = os.path.splitext(config_file)
+                new_filename = fn_root + TEMP_TAG + fn_ext
+            with open(new_filename,'w') as f:
+                f.writelines(newlines)
+
+###############################################################################
+def edit_file_metadata(files, std_nm_replacements, overwrite):
+###############################################################################
+    
+    for file in files:
+        #it is assumed that we start with a list of source files (not .meta files), so construct the .meta filename
+        (fn_root, fn_ext) = os.path.splitext(file)
+        meta_filename = fn_root + '.meta'
+        
+        if os.path.isfile(meta_filename):
+            #read in entire file contents
+            with open(meta_filename, 'r') as f:
+                contents = f.read()
+            
+            total_replacements = 0
+            for r in std_nm_replacements:
+                #replace each standard name (whole word only) in the entire contents of the file and keep track of how many names have been replaced
+                (contents, n_replacements) = re.subn(r'\b%s\b' % r[0], r[1], contents)
+                total_replacements += n_replacements
+            
+            #write out edited file if necessary, using either a temporary new metadata file or the existing metadata file
+            if total_replacements > 0:
+                if overwrite:
+                    new_filename = meta_filename
+                else:
+                    (fn_root, fn_ext) = os.path.splitext(meta_filename)
+                    new_filename = fn_root + TEMP_TAG + fn_ext
+                with open(new_filename,'w') as f:
+                    f.write(contents)
+    
+###############################################################################
+def main_func():
+###############################################################################
+    """Describe ...
+    """
+    # Parse command line arguments
+    args = parse_command_line(sys.argv[1:], __doc__)
+    
+    
+    #Use existing functionality from the ccpp_prebuild.py script to return host + scheme metadata
+    builddir = None
+    (success, config) = import_config(args.config_file, builddir)
+    if not success:
+        raise Exception('Call to import_config failed.')
+    
+    #add relative paths to variable definition files and scheme files
+    for idx, x in enumerate(config['variable_definition_files']):
+        if not os.path.isfile(x):
+            config['variable_definition_files'][idx] = args.host_base + '/' + x
+    
+    for idx, x in enumerate(config['scheme_files']):
+        if not os.path.isfile(x):
+            config['scheme_files'][idx] = args.host_base + '/' + x
+    
+    # Variables defined by the host model
+    (success, metadata_define, dependencies_define) = gather_variable_definitions(config['variable_definition_files'], config['typedefs_new_metadata'])
+    if not success:
+        raise Exception('Call to gather_variable_definitions failed.')
+    
+    # Variables requested by the CCPP physics schemes
+    (success, metadata_request, arguments_request, dependencies_request, schemes_in_files) = collect_physics_subroutines(config['scheme_files'])
+    if not success:
+       raise Exception('Call to collect_physics_subroutines failed.')
+    
+    stdname_file = os.path.abspath(args.standard_name_file)    
+    _, root = read_xml_file(stdname_file)
+        
+    #get list of all standard names from the standard name file
+    all_std_names = []
+    for name in root.findall('./section/standard_name'):
+        all_std_names.append(name.attrib['name'])
+    
+    if (args.mode == 'u'):
+        #just check for standard names that are in host + physics metadata but NOT in the standard name library
+        check_unknown_vars(all_std_names, metadata_define, metadata_request, args.standard_name_file)
+    elif (args.mode == 'c' or args.mode == 'm'):
+        #determine additions, replacements, and removals for standard names given two standard name library files
+        (std_name_changes, n_replacements, n_adds, n_removals) = find_changed_vars(args.old_standard_name_file, args.standard_name_file)
+        
+        #print out results
+        print('Detected {0} changes ({1} replacements, {2} additions, {3} removals) between {4} and {5}:'.format(len(std_name_changes),n_replacements,n_adds,n_removals,args.old_standard_name_file,args.standard_name_file))
+        for chg in std_name_changes:
+            if not chg[0]:
+                print('NEW: {}'.format(chg[1]))
+            elif not chg[1]:
+                print('RM: {}'.format(chg[0]))
+            else:
+                print('CHG: {} -> {}'.format(chg[0],chg[1]))
+        
+        #write out results
+        if JSON_CHG_FILE:
+            with open(JSON_CHG_FILE, 'w') as filehandle:
+                json.dump(std_name_changes, filehandle)
+    
+    if (args.mode == 'm'):
+        #for editing metadata files, only handle replacements for now (could extend to handle removals if necessary)
+        std_nm_replacements = [x for x in std_name_changes if x[0] and x[1]]
+        
+        #check for and edit changed standard names in config file
+        edit_config_metadata(args.config_file, std_nm_replacements, args.overwrite)
+        #check for and edit changed standard names in host and physics metadata
+        edit_file_metadata(config['variable_definition_files'], std_nm_replacements, args.overwrite)
+        edit_file_metadata(config['scheme_files'], std_nm_replacements, args.overwrite)
+        
+###############################################################################
+if __name__ == "__main__":
+    main_func()
